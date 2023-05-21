111635d6e0fccc9b1efa4992fd4a402b1c447a81
goyalyash28 <65063745+goyalyash28@users.noreply.github.com>
2023-05-21 22:48:21 +0530

    Add files via upload

---
+++ src/test/java/com/retailApp/CartService/CartServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package com.retailApp.CartService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.retailApp.CartService.Repository.CartDao;
+
+@SpringBootTest
+class CartServiceApplicationTests 
+{
+
+
+}
